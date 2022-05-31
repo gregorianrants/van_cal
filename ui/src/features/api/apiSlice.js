01c4fcd4f9144452bfd28f2ef386ad9e1372df18
gregor murray <gregorian_rants@hotmail.com>
2022-06-01 00:23:52 +0100

    made a change

--- ui/src/features/api/apiSlice.js
+++ ui/src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 // Import the RTK Query methods from the React-specific entry point
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const BASE_URL = process.env.REACT_APP_SERVER_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 console.log(BASE_URL)
 
