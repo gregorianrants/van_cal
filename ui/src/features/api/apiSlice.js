a31c071d83d38924765fe8eeee50029e75c32548
gregor murray <gregorian_rants@hotmail.com>
2022-05-31 21:19:10 +0100

    made a change

--- ui/src/features/api/apiSlice.js
+++ ui/src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 // Import the RTK Query methods from the React-specific entry point
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://dry-earth-66864.herokuapp.com'
+const BASE_URL = process.env.REACT_APP_SERVER_URL
 
 console.log(BASE_URL)
 
