1aec59a257da54e24ee0fb09784e1260cac263ae
gregor murray <gregorian_rants@hotmail.com>
2022-06-01 01:07:44 +0100

    made a change

--- ui/src/features/googleCalendar/gcalApi.js
+++ ui/src/features/googleCalendar/gcalApi.js
@@ -1,11 +1,12 @@
 import auth0Client from "../auth/auth0";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export async function authorize(code){
     const auth0 = await auth0Client;
     const token = await auth0.getTokenSilently();
 
-    return await fetch(`http://localhost:8000/api/v1/gcal/authourize?code=${code}`, {
+    return await fetch(`${BASE_URL}/api/v1/gcal/authourize?code=${code}`, {
         headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
