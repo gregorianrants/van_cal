12bd84bc026c1008a9734d243fa3904191f9f00e
gregor murray <gregorian_rants@hotmail.com>
2022-06-16 17:40:01 +0100

    changed styling on home page

--- ui/src/features/HomePage.js
+++ ui/src/features/HomePage.js
@@ -78,16 +78,9 @@ export default function(){
             <Typography variant='h5'>The solution</Typography>
            <Paragraph>The solution is a SAAS (Software As A Service) application that any Man With A Van business can use.  The user can sign in with an email and password or with their google account and create and manage bookings in the application. The bookings are displayed on a week view calendar to facilitate scheduling. the data is recorded at a level of atomicity that means the information captured when making a booking can be used for business record keeping.</Paragraph>
 
-           <Paragraph>Here is a short video demonstrating making a booking then changing the time of the booking by dragging the event tile.  The red tiles are Bookings and the blue tile is an event from the users google calendar.</Paragraph>
 
-            <VideoContainer>
-                <video width='800px' controls>
-                    <source src={video}
-                            type="video/mp4"/>
-                </video>
-            </VideoContainer>
 
-           <Paragraph>Users can also create invoice db records on the application api, from the booking record and email invoices to customers from the application. They can optionally provide their email address and company details in the settings page and this will be used to generate an invoice with the booking details and the company details. the sent invoice will be sent from a no reply email address which belongs to VanCal. However the reply-to field of the email will be set to the users email address so that if a customer replies to the email the application user will recieve the email.</Paragraph>
+           <Paragraph>Users can also create, manage and email invoices from the application.  Users can optionally provide an email address, business name and business adress on the settings page and this will be used to create an invoice with the correct business information for the user.  The invoice is sent from a noreply email address managed by the developer of VanCal but if customers of the user reply to the email the reply will be sent to the email address provided by the user in settings.</Paragraph>
 
             <Typography variant='h5'>Integration with google calendar</Typography>
            <Paragraph>VanCal provides an option to authorise the application to read the event data on a users Google Calendar.  This allows a user to view other events they may have going on in the same place that they make bookings for their business.  As someone who runs a man with a van business I find this a very useful feature.  However users can decide themselves if this feature is usefull to them and opt in or out as they choose</Paragraph>
@@ -96,15 +89,15 @@ export default function(){
 
             <Typography variant='h5'>Users Data</Typography>
 
-            <h3>Google Calendar Users Data</h3>
+            <Typography variant='h6'>Google Calendar Users Data</Typography>
            <Paragraph>other than fetching a users event data from google calendar and displaying it on the ui of the application van cal does not store, share or use for any purpose a users google calendar data.  more information on our data policies can be found on our <Link to='/privacy-policy'>privacy policy</Link> page</Paragraph>
 
-            <h3>Data Users Create for bookings</h3>
+            <Typography variant='h6'>Data Users Create for bookings</Typography>
            <Paragraph>The application is currently for demonstration purposes only and any data provided for bookings is expected to be dummy data and this data will be deleted at regular intervals and will persist for no longer than 1 month.</Paragraph>
            <Paragraph>The application is currently for demonstration purposes only and any data provided for bookings is expected to be dummy data and this data will be deleted at regular intervals and will persist for no longer than 1 month.</Paragraph>
            <Paragraph>This data is stored on the appications api database, the data is secured using Oauth2 via the Auth0 authorization service. this data is only stored so that i can be managed by the user of the application for their own benefit. the application developer does not use this data for any other purpose.  The data is not shared with any 3rd parties.  At the moment the application is for demonstration purposes only</Paragraph>
 
-            <h3>Email, Company Name and Company Address data</h3>
+            <Typography variant='h6'>Email, Company Name and Company Address data</Typography>
            <Paragraph>This data can be provided for the purpose of populating fields of invoices sent from the application, this data is only used for the functionality it provides in the application. Providing this information is completely optional and much of the functionality of the app can be used without providing this data.</Paragraph>
         </Container>
 
